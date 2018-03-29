e2734991c97d86b736bbe0163375e0bad8d6bf7f
Abdoulaye <abdoulaye02539@gmail.com>
2018-03-29 16:14:58 +0000

    EXAMEN L2 Abdoulaye DIAO

---
+++ Formulaire.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Adresse client</title>
+   <META charset ="utf-8"/>
+   <link rel="stylesheet" type="text/css" href="fichier.css">
+
+</head>
+<body>
+	<div>
+		<h1 style="..">Veillez renseignez ces informations</h1>
+<form method="post" action="traitement.php></form>
+<label for="name"> Votre nom:</label><br>
+<input type="text" name="nom" id="nom"/><br>
+<br>
+<label for="name"> Votre Prenom:</label><br>
+<input type="text" name="prenom" id="prenom"/><br>
+<br>
+
+<label for="name"> Votre Adresse:</label><br>
+<input type="text" name="Adresse" id="Adresse"/><br>
+<br>
+
+
+<input type="submit" value="Enregistrer">
+</div>
+
+</body>
+</html>
